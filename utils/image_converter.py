32ee3f952dd7fd84e20420d1566f4510128f2cdb
Saif <skh9588@g.rit.edu>
2021-11-27 03:48:28 +0400

    Image conversion script resolution increased to 150%, format changed to png.

--- utils/image_converter.py
+++ utils/image_converter.py
@@ -3,20 +3,24 @@ import sys
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='convert pdf to images')
-parser.add_argument('filePath')
-args = parser.parse_args()
-images = convert_from_path(args.filePath)
 
-output_dir = os.path.join(os.getcwd(),"Output_images")
+# Image to PDF conversion script, takes 2 arguments: File name and output folder name
+
+parser = argparse.ArgumentParser(description='convert pdf to images')
+parser.add_argument('-f','--fileName', help="PDF File name", type=str, required=True)
+parser.add_argument('-o','--output', help="Output directory", type=str, required=True)
+args = parser.parse_args()
+images = convert_from_path(args.fileName, dpi=300, thread_count = 8)
+
+output_dir = os.path.join(os.getcwd(),args.output)
 os.mkdir(output_dir)
 os.chdir(output_dir) 
 
-print(f"Converting file {args.filePath} from pdf to image .. please wait")
+print(f"Converting file {args.fileName} from pdf to image .. please wait")
 
 
 for i in range(len(images)):
     # Save pages as images in the pdf
-    images[i].save('page' + str(i) + '.jpg', 'JPEG')
+    images[i].save('page' + str(i) + '.png', 'png')
 
-print(f"File {args.filePath} converted, total of {i} images creates")
+print(f"File {args.fileName} converted, total of {i+1} images creates")
