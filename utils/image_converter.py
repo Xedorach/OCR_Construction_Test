02d5dc73ce7bc2eb1bf549406c3e33faa4e07462
Saif <skh9588@g.rit.edu>
2021-11-27 04:15:19 +0400

    Updated Requirements to include image_slicer, added image slicing capability to image converter script

--- utils/image_converter.py
+++ utils/image_converter.py
@@ -2,14 +2,22 @@ from pdf2image import convert_from_path
 import sys
 import argparse
 import os
-
+from image_slicer import slice
 
 # Image to PDF conversion script, takes 2 arguments: File name and output folder name
+# Different arguments that can be passed to convert_from_path:
+
+# convert_from_path(pdf_path, dpi=200, output_folder=None, first_page=None, last_page=None,
+#  fmt='ppm', jpegopt=None, thread_count=1, userpw=None, use_cropbox=False, strict=False,
+#  transparent=False, single_file=False, output_file=str(uuid.uuid4()), poppler_path=None, 
+# grayscale=False, size=None, paths_only=False, use_pdftocairo=False, timeout=600)
 
 parser = argparse.ArgumentParser(description='convert pdf to images')
 parser.add_argument('-f','--fileName', help="PDF File name", type=str, required=True)
 parser.add_argument('-o','--output', help="Output directory", type=str, required=True)
-parser.add_argument('-t','--threadCount', help="Output directory", type=int, const = 2)
+parser.add_argument('-t','--threadCount', help="Output directory",nargs='?', type=int, const = 2)
+parser.add_argument('-s','--slice',help='How many slices for the image',nargs='?', type=int,
+                     const=1)
 args = parser.parse_args()
 images = convert_from_path(args.fileName, dpi=300, thread_count = args.threadCount)
 
@@ -22,6 +30,8 @@ print(f"Converting file {args.fileName} from pdf to image .. please wait")
 
 for i in range(len(images)):
     # Save pages as images in the pdf
+    out_img = (f'page{i}.png')
     images[i].save('page' + str(i) + '.png', 'png')
+    slice(out_img, args.slice)
 
-print(f"File {args.fileName} converted, total of {i+1} images creates")
+print(f"File {args.fileName} converted, total of {i+1} images created with {args.slice} number of slices")
