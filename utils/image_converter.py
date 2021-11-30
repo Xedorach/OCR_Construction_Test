fb19097710eedd1fdd27e1db8b41689f70ea25ab
Saif <skh9588@g.rit.edu>
2021-11-30 06:11:25 +0400

    Added easy OCR

--- utils/image_converter.py
+++ utils/image_converter.py
@@ -8,8 +8,8 @@ from image_slicer import slice
 # Different arguments that can be passed to convert_from_path:
 
 # convert_from_path(pdf_path, dpi=200, output_folder=None, first_page=None, last_page=None,
-#  fmt='ppm', jpegopt=None, thread_count=1, userpw=None, use_cropbox=False, strict=False,
-#  transparent=False, single_file=False, output_file=str(uuid.uuid4()), poppler_path=None, 
+# fmt='ppm', jpegopt=None, thread_count=1, userpw=None, use_cropbox=False, strict=False,
+# transparent=False, single_file=False, output_file=str(uuid.uuid4()), poppler_path=None, 
 # grayscale=False, size=None, paths_only=False, use_pdftocairo=False, timeout=600)
 
 parser = argparse.ArgumentParser(description='convert pdf to images')
