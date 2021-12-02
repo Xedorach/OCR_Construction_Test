493d036441d7c3f8974856ccdb951447e3a66785
Saif <skh9588@g.rit.edu>
2021-12-02 10:22:35 +0400

    Working easy_ocr and image conversion

--- utils/image_converter.py
+++ utils/image_converter.py
@@ -4,13 +4,15 @@ import argparse
 import os
 from image_slicer import slice
 
-# Image to PDF conversion script, takes 2 arguments: File name and output folder name
-# Different arguments that can be passed to convert_from_path:
+'''
+Image to PDF conversion Arguments
+Different arguments that can be passed to convert_from_path:
 
-# convert_from_path(pdf_path, dpi=200, output_folder=None, first_page=None, last_page=None,
-# fmt='ppm', jpegopt=None, thread_count=1, userpw=None, use_cropbox=False, strict=False,
-# transparent=False, single_file=False, output_file=str(uuid.uuid4()), poppler_path=None, 
-# grayscale=False, size=None, paths_only=False, use_pdftocairo=False, timeout=600)
+convert_from_path(pdf_path, dpi=200, output_folder=None, first_page=None, last_page=None,
+fmt='ppm', jpegopt=None, thread_count=1, userpw=None, use_cropbox=False, strict=False,
+transparent=False, single_file=False, output_file=str(uuid.uuid4()), poppler_path=None, 
+grayscale=False, size=None, paths_only=False, use_pdftocairo=False, timeout=600)
+'''
 
 parser = argparse.ArgumentParser(description='convert pdf to images')
 parser.add_argument('-f','--fileName', help="PDF File name", type=str, required=True)
